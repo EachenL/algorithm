30d5ddacbedbda28399780bd23debe783867cf1a
Mark Ning <shundyning@163.com>
2018-04-10 21:31:08 +0800

    new add InsertionSort

---
+++ InsertionSort.cpp
@@ -0,0 +1,16 @@
+//
+// Created by markning on 4/10/18.
+//
+void InsetionSort(int a[], int size) {
+    for (int i = 1; i < size; i++) {
+        for (int j = 0; j < i; j++)
+            if(a[j] > a[i]) {
+                int tmp = a[i];
+                for (int k = i; k > j; k--)
+                    a[k] = a[k-1];
+                a[j] = tmp;
+                break;
+            }
+
+    }
+}
---
+++ SelectionSort.cpp
@@ -0,0 +1,14 @@
+//
+// Created by markning on 4/8/18.
+//
+void SelectionSort (int a[], int size) {
+    for (int i = 0; i < size - 1; i++) {//此语句共执行size-1次
+        int tmpMin = i;//tmpMin作为i与j比较过程中的指针，在比较之后指向小的数
+        for (int j = i + 1; j < size; j++){//j永远比i大1
+            if (a[j] < a[tmpMin])
+        }
+    }
+    int tmp = a[i];//将本次循环中两数的最小值移到左边
+    a[i] = a[tmpMin];//
+    a[tmpMin] = tmp;//
+}
